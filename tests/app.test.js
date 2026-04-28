c936cf3ae4e789f54dbb7d653be65b8614d986d2
Omkar <omkarkadu66527@gmail.com>
2026-04-28 17:47:25 +0530

    run CI with tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,3 +5,8 @@ test("GET / returns 200", async () => {
   const res = await request(app).get("/");
   expect(res.statusCode).toBe(200);
 });
+
+test("GET /unknown returns 404", async () => {
+  const res = await request(app).get("/unknown");
+  expect(res.statusCode).toBe(404);
+});
